4b09d91940640c08a4f4e0b9fd037a066b43d10b
Aurel Adestira <aurelsalsaa75@gmail.com>
2026-03-31 13:08:58 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Aurel Adestira Salsabila Modul5_103082400041/Program.cs
+++ Aurel Adestira Salsabila Modul5_103082400041/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T a, T b, T c)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+        return x + y + z;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan p = new Penjumlahan();
+
+        float hasil = p.JumlahTigaAngka<float>(10f, 30f, 82f);
+        Console.WriteLine("Hasil penjumlahan: " + hasil);
+        Console.ReadKey();
+    }
+}
